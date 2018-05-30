3b48c0bf5fc4eaedd4aade89e04e07d0f1e785aa
Amanda Kielley <eatingfoil@gmail.com>
2018-05-30 04:47:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
   if int % 15 == 0 
     "FizzBuzz"
   end
-  if int % 3 == 0 
+  elsif int % 3 == 0 
     "Fizz" 
   end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   end
 end
